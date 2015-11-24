18261af61985f3be1cc08610a0485668dc5673dc
cyrozap <cyrozap@gmail.com>
2015-11-24 00:26:17 -0500

    This removes the residual event listeners

--- panelook-select-fix.js
+++ panelook-select-fix.js
@@ -3,3 +3,9 @@ var body = document.getElementsByTagName("body")[0];
 for (i = 0; i < attributes.length; i++) {
     body.removeAttribute(attributes[i]);
 }
+
+var new_body = body.cloneNode(true);
+
+var html = body.parentElement;
+html.removeChild(body);
+html.appendChild(new_body);
